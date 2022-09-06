3da1907e2694ab5e857ba666f28a8dd6b294ff29
Jairo Styp Rodriguez <83678813+styp07@users.noreply.github.com>
2022-09-05 23:18:33 -0500

    Add METODOS DE STRING

---
+++ 01_MODULO1/12_METODOS1/index.js
@@ -0,0 +1,138 @@
+/* 
+
+Métodos string
+
+    concat() - Junta dos o mas cadenas de texto
+
+    starWith() - Valida si una cadena comienza con los caracteres de otra cadena y devuelve true o false
+    endsWith() - Valida si una cadena termina con los caracteres de otra cadena y devuelve true o false
+    includes() - Valida si una cadena esta con los caracteres de otra cadena y devuelve true o false
+    indexOf() - Devuelve el indice del primer carácter de la cadena, si no existe devuelve -1
+    lastIndexOf() - Devuelve el ultimo indice del primer carácter de la cadena, si no existe devuelve -1
+
+    padStard() - Rellena una cadena al principio con caracteres deseados
+    padEnd() - Rellena una cadena al final con caracteres deseados
+    repeat() -Devuelve la misma cadena, pero repetida la cantidad de veces deseada
+
+    split() - Divide la cadena como lo pidamos
+    substring() - Nos retorna un pedazo de la cena que seleccionamos
+    toLowerCase() - Convierte una cadena a minúscula
+    toUpperCase() - Convierte una cadena a mayúscula
+    toString() - Método que devuelve una cadena que representa al objeto especificado
+    trim() - Elimina los espacios en blanco al principio y al final de una cadena 
+    trimStart() - Elimina los espacios en blanco al comienzo de un cadena
+    trimEnd() - Elimina los espacios en blanco al final de una cadena
+    valueOf() - Retorna el valor primitivo de un objeto String
+
+
+Métodos arrays
+
+    pop() - Elimina el ultimo elemento de un array
+    
+*/
+
+// // concat -----------------------------------------------------------
+// let texto1 = 'Hola soy '
+// let texto2 = 'Jairo'
+// let resultado = texto1.concat(texto2)
+// console.log(resultado)
+
+
+// // startWith
+
+// let texto1 = 'Hola soy Jairo y estudio Javascript'
+// let texto2 = 'Hola'
+// let resultado = texto1.startsWith(texto2)
+// console.log(resultado);
+
+// // endsWith
+
+// let texto1 = 'Hola soy Jairo y estudio Javascript';
+// let texto2 = 'Javascript'
+// let resultado = texto1.endsWith(texto2)
+// console.log(resultado)
+
+// // includes
+
+// let texto1 = 'Hola soy Jairo y estudio Javascript'
+// let texto2 = 'Jairo'
+// let resultado = texto1.includes(texto2)
+// console.log(resultado)
+
+// // indexOf
+
+// let texto1 = 'Hola soy Jairo y estudio Javascript'
+// let resultado = texto1.indexOf('estudio')
+// console.log(resultado)
+
+// // lastIndexOf
+
+// let texto1 = 'Hola soy Jairo y estudio Javascript'
+// let resultado = texto1.lastIndexOf('estudio')
+// console.log(resultado)
+
+// // padStart
+
+// let texto1 = 'Hola'
+// let resultado = texto1.padStart('20', 'xd ')
+// console.log(resultado)
+
+// // padEnd
+
+// let texto1 = 'Hola'
+// let resultado = texto1.padEnd('20', ' xd')
+// console.log(resultado)
+
+// // repeat
+// let texto1 = 'Hola '
+// let resultado = texto1.repeat('5')
+// console.log(resultado)
+
+// // split
+// let texto1 = 'Hola,mucho,gusto'
+// let resultado = texto1.split(',');
+// console.log(resultado[1])
+
+// // substring
+// let texto1 = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+// let resultado = texto1.substring(2,10);
+// console.log(resultado)
+
+// // toLowerCase
+// let texto1 = 'ABcDeFGHIjkLMnÑOpqRSTuVWxyZ'
+// let resultado = texto1.toLowerCase();
+// console.log(resultado)
+
+// // toUpperCase
+// let texto1 = 'ABcDeFGHIjkLMnÑOpqRSTuVWxyZ'
+// let resultado = texto1.toUpperCase();
+// console.log(resultado)
+
+// // toString
+// let texto1 = 245;
+// console.log(typeof(texto1));
+// let resultado = texto1.toString();
+// console.log(typeof resultado);
+
+// // trim
+// let texto1 = '          xd          '
+// console.log(texto1.length)
+// let resultado = texto1.trim();
+// console.log(resultado.length) 
+
+// // trimStart
+// let texto1 = '          xd          '
+// console.log(texto1.length)
+// let resultado = texto1.trimStart();
+// console.log(resultado.length) 
+
+// // trimEnd
+// let texto1 = '          xd          '
+// console.log(texto1.length)
+// let resultado = texto1.trimEnd();
+// console.log(resultado.length) 
+
+
+// // valueOf
+// var Student = { Name: 'Ali',  RollNo: '234' }
+// console.log(Student.valueOf()) 
