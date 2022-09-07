59dd874b2c9e34c92fb8e96d63b401e3c2a8b6d9
Jairo Styp Rodriguez <83678813+styp07@users.noreply.github.com>
2022-09-06 20:22:37 -0500

    Add OBJETO MATH

---
+++ 01_MODULO1/13_OBJETO_MATH/index.js
@@ -0,0 +1,127 @@
+/* 
+
+Métodos
+
+    sqrt() - Devuelve la raíz cuadrada positiva de un número
+    cbrt() - Devuelve la raíz cubica de un número
+    max() - Devuelve el mayor de cero a mas números
+    min() - Devuelve el mas pequeño de cero o más números
+    random() - Devuelve un número pseudo-aleatorio entre 0 y 1
+    round() - Devuelve el valor de un número redondeado al número entero mas cercano
+    fround() - Devuelve la representación flotante de precisión simple mas cercana de un número
+    floor() - Devuelve el mayor entero menor que o igual a un número
+    trunc() - Devuelve la parte entera del número x, la eliminación de los dígitos fraccionarios 
+
+Propiedades
+
+    PI - Radio de la circunferencia de un circulo respecto a su diámetro
+    SQRT1_2 - Raiz cuadrada de 1/2 
+    SQRT2 - Raiz cuadrada de 2
+
+    E - Constante de Euler, la base de los logaritmos naturales
+    LN2 - Logaritmo natural de 2
+    LN10 - Logaritmo natural de 10
+    LOG2E - Logaritmo de E con base 2
+    LOG10E - Logaritmo de E con base 10
+
+*/
+
+// // sqrt
+
+// let numero = Math.sqrt(25);
+// console.log(numero);
+
+
+// // cbrt
+
+// let numero = Math.cbrt(27);
+// console.log(numero);
+
+
+// // max
+
+// let numero = Math.max(25,2,4,52,65,84,4,13,50);
+// console.log(numero);
+
+
+// // min
+
+// let numero = Math.min(25,2,4,52,65,84,4,13,50);
+// console.log(numero);
+
+
+// // random
+// let numero = Math.random();
+// console.log(numero);
+
+
+// // round
+
+// let numero = Math.round(Math.random()*100);
+// console.log(numero);
+
+
+// // floor
+
+// let numero = Math.floor(Math.random()*99);
+// console.log(numero);
+
+
+// // fround
+
+// let numero = Math.fround(9.55555555455554544);
+// console.log(numero);
+
+
+// // trunc
+
+// let numero = Math.trunc(9.146541651651646541651);
+// console.log(numero);
+
+
+// // PI
+
+// let num = Math.PI;
+// document.write(num)
+
+
+// // SQRT1_2
+
+// let num = Math.SQRT1_2;
+// document.write(num)
+
+
+// // SQRT2
+
+// let num = Math.SQRT2;
+// document.write(num)
+
+
+// // E
+
+// let num = Math.E;
+// document.write(num)
+
+
+// // LN2
+
+// let num = Math.LN2;
+// document.write(num)
+
+
+// // LN10
+
+// let num = Math.LN10;
+// document.write(num)
+
+
+// // LOG2E
+
+// let num = Math.LOG2E;
+// document.write(num)
+
+
+// // LOG10E
+
+// let num = Math.LOG10E;
+// document.write(num)
