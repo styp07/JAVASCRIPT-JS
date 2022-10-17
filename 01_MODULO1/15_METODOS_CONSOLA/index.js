6000109aa3d141d032535bc59b48d72adebd6432
Jairo Styp Rodriguez <83678813+styp07@users.noreply.github.com>
2022-10-16 20:25:35 -0500

    Add METODOS_CONSOLA

---
+++ 01_MODULO1/15_METODOS_CONSOLA/index.js
@@ -0,0 +1,30 @@
+/* 
+
+    assert() Aparece un mensaje de error en la consola si la información es falsa
+    clear() Limpia la consola
+    error() Muestra un error en la consola 
+    info() Emite un mensaje informativo 
+    log() Muestra un mesnaje en la consola
+    table() Toma un argumento obligatorio: data, que debe ser un array o un objeto 
+    warn() Mensaje de advertencua
+    dir() Lista de las propiedades de un objeto 
+
+FUNCION DE CONTEO
+
+    count() Registra el número de veces que se llama count()
+    countreset() Resetea el contador console.count()
+
+
+FUNCIONES DE AGRUPACIÓN
+
+    group() Crea un grupo en la nueva linea de registro 
+    groipEnd() Remueve el grupo
+    groupCollapsed() Crea un grupo contraido
+
+FUNCIONES DE TIEMPO 
+
+    time() Inicia un temporizador
+    timeEnd() Registra el valor actual del temporizador
+    timeLog() Detiene el temporizador
+
+*/
