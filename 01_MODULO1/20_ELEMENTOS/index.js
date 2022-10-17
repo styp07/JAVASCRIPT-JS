b2bb909a1d9dff07d9c99aa9767bbac63fe21cf7
Jairo Styp Rodriguez <83678813+styp07@users.noreply.github.com>
2022-10-17 16:06:54 -0500

    Add ELEMENTOS

---
+++ 01_MODULO1/20_ELEMENTOS/index.js
@@ -0,0 +1,23 @@
+// const text = document.querySelector('.title');
+
+// let resultado1 = text.textContent;
+// let resultado2 = text.innerHTML;
+// let resultado3 = text.outerHTML;
+
+// alert(resultado1);
+// alert(resultado2);
+// alert(resultado3);
+
+
+// const contenedor = document.querySelector('.container')
+
+// const fragmento = document.createDocumentFragment();
+
+// for (let i = 0; i < 20; i++) {
+//     const item = document.createElement('LI');
+//     item.innerHTML = 'Este es un item de la lista';
+//     fragmento.appendChild(item);
+// }
+
+// contenedor.appendChild(fragmento);
+// console.log(contenedor);
