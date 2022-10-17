3dbfab72c792bfe2fabb797db266a9e787a3a7fa
Jairo Styp Rodriguez <83678813+styp07@users.noreply.github.com>
2022-10-17 13:06:26 -0500

    Add ATRIBUTOS

---
+++ 01_MODULO1/18_ATRIBUTOS/index.js
@@ -0,0 +1,29 @@
+// setAtribute() - Modifica valor de atributo
+// getAtribute() - Obtiene el valor de un atributo
+// removeAtribute() - Remueve el valor de un atributo
+
+
+const text = document.querySelector('.text');
+const modificador = document.getElementById('modificar');
+
+// document.write(text.getAttribute('type'))
+
+// text.setAttribute('placeholder', 'hola xd')
+
+// text.setAttribute('type','number')
+
+// document.write(text.getAttribute('type'))
+
+// text.removeAttribute('placeholder')
+
+
+
+
+// Atributos globales -----------------------------------------------------------------
+
+text.placeholder = 'XD';
+text.required = 'true';
+
+// Atributos style --------------------------------------------------------------------
+
+modificar.style.color = 'orange';
